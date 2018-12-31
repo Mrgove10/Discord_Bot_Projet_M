af6a923ca45769c1fda283873a1776a15d5a2a9b
A.R <richardadrien0@gmail.com>
2019-01-01 00:16:40 +0100

    changes playig whit and added comments

--- app/Main_Melius.js
+++ app/Main_Melius.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 const schedule = require('node-schedule');
 
 //Files
-const phraseObj = require("./phrase.json")
+const phraseObj = require("./phrase.json") 
 const config = require("./config.json");
 const JsonPackage = require('.././package.json');
 const client = new Discord.Client();
@@ -11,54 +11,69 @@ const client = new Discord.Client();
 //Variables
 var lastrandom = 0
 client.on("ready", () => {
+  //debug
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 
-  client.user.setActivity(`Hacker le bank`); //met a jour le "playing whit"
+  //Met a jour le playing whit au lancement
+  client.user.setActivity(`Hacker le bank`); 
 
-  //#region envoie automatique
-  //https://www.npmjs.com/package/node-schedule
+  //#region 
+  //envoie automatique (https://www.npmjs.com/package/node-schedule)
+
+  //envoie automatique du matin
   schedule.scheduleJob('0 55 7 * * *', function () {
     client.channels.get("387249474625601537").send('euhhh oui, Bonjour, bonne chournéé');
   });
   
+  //envoie automatique du soir
   schedule.scheduleJob('0 45 21 * * *', function () {
     client.channels.get("387249474625601537").send('euhhh oui, Bonne nuit, demain caféé douceur');
   });
 
+  //envoie automatique tous les mecredi
   schedule.scheduleJob('0 15 8 * * 3', function () {
     client.channels.get("387249474625601537").send("", {
       file: "./app/media/wednesday.jpg" // 
     });
   });
 
+  //changement automatique du playing whit
   schedule.scheduleJob('* * * 1 * *', function () {
-    var mi = makeid();
-    client.user.setActivity(`Hacker le bank : ` + mi); //met a jour le "playing whit"
+    var id = makeid();
+    var idd = makeid();
+    client.user.setActivity(id + ` Hacker le bank ` + idd); //met a jour le "playing whit"
     console.log(makeid());
   });
   //#endregion
 });
 
+//for each message
 client.on("message", async message => {
+  //Debug
+  if (!message.author.bot) { //if the message is not from the bot
+    console.log("Message recu : " + message.content); 
+  }
 
+  //emoji oui
   var Emoji_Oui = message.guild.emojis.find(x => x.name === "oui");
-  /*message.react(Emoji_Oui);*/
+  
+  //si le bot est mentionner il repond oui ou non
   if (message.isMentioned(client.users.get('494062611810484224'))) {
-    var rep = ["euhhh oui", "euhhh non"];
+    var rep = ["euhhh oui", "euhhh non", "euhhh peut-etre" ];
     message.reply(rep[getRandomInt(2)]);
   }
 
-  if (!message.author.bot) {
-    console.log("Message recu : " + message.content); //debug
-  }
-  
-  if (message.author.bot) return; //si le message est vide
+  //si le message est vide
+  if (message.author.bot) return; 
 
-  if (message.content.indexOf(config.prefix) !== 0) return; // si la commande c'est pas au debut
+   // si la commande c'est pas au debut
+  if (message.content.indexOf(config.prefix) !== 0) return;
 
+  //recupere les arguments puis la met en minucule
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
+  //Help commande
   if (command === "help" || command === "?" || command === "aide") {
     message.channel.send(`Toute les commandes doivent etre prifixé par "/m"
     Les commandes disponible sont: 
@@ -71,14 +86,17 @@ client.on("message", async message => {
     `);
   }
 
+  //Ping commande
   if (command === "ping") {
     message.channel.send(`Pong!`);
   }
 
+  //Music commande
   if (command === "musique") {
     message.channel.send('https://soundcloud.com/nicolas-asri/shutrap/s-onxUc');
   }
 
+  //Oui commande
   if (command === "oui") {
 
     var i = getRandomInt(phraseObj.ListPhrase.length);
@@ -93,10 +111,12 @@ client.on("message", async message => {
     var lastrandom = i
   }
 
+  //Shitbook commande
   if (command === "shitbook") {
     message.channel.send('https://testing.adrienrichard.com/YearBook/');
   }
 
+  //About commande
   if (command === "about") {
 
     var time = process.uptime(); //temps depuis le demararge
@@ -105,6 +125,7 @@ client.on("message", async message => {
     var version = JsonPackage.version; //recuperation de la version de l'appli
 
     var m = await message.channel.send("Ping en cours..."); //envoie du message
+
     //Mofification du message
     m.edit(`
     ----- MeliusBot v${version} -----
@@ -119,12 +140,15 @@ client.on("message", async message => {
   }
 });
 
+//Logins the client
 client.login(config.token);
 
+//Get a random int 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
+//
 String.prototype.toHHMMSS = function () {
   var sec_num = parseInt(this, 10); // don't forget the second param
   var days = Math.floor(sec_num / (3600 * 24));
@@ -148,6 +172,7 @@ String.prototype.toHHMMSS = function () {
   return time;
 }
 
+//Create a random ID
 function makeid() {
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
